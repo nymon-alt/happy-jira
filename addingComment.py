b3202632f77f04dc5ab7ca2f7f3168abff424a6d
Szymon Natanek <natanek402@gmail.com>
2026-04-10 12:13:36 +0200

    comment to existing task

---
+++ addingComment.py
@@ -0,0 +1,42 @@
+#dodawanie komentarza do wczesniej utworzonego zadania
+
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+
+url = "https://nymon.atlassian.net/rest/api/3/issue/HAP-3/comment" #wpisujesz ID zadania bezposrednio, nie calosc
+
+auth = HTTPBasicAuth("natanek402@gmail.com", "ATATT3xFfGF0VCQLnTOu2qk2k38D6oFWpJD9cLya6QeIV9msPYShSpyzeBgDnyGQKsb3kTdgd52GCTQY9o1dcgPVmQk9Zlne0LsJegeJKMjmV7NXPAyckTzv_zv9KEsWsQV2bF9g3JsJTuE_6feIyTsVI4ZA71HY4twWh3OLjrcjxfUHDZxVhP0=93D16BCD") #cholernie glupie
+
+headers = {
+  "Accept": "application/json",
+  "Content-Type": "application/json"
+}
+
+payload = json.dumps( {
+    "body": {
+    "type": "doc",
+    "version": 1,
+    "content": [
+      {
+        "type": "paragraph",
+        "content": [
+          {
+            "text": "jestem bogiem tej gierki",
+            "type": "text"
+          }
+        ]
+      }
+    ]
+  }
+} )
+
+response = requests.request(
+   "POST",
+   url,
+   data=payload,
+   headers=headers,
+   auth=auth
+)
+
+print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
